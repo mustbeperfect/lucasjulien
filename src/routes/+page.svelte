9b1c42eab5e6cdabacadd8bc9d40fb7f4d46a07a
Lucas <169063936+mustbeperfect@users.noreply.github.com>
2025-02-16 21:40:28 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    //import "https://kit.fontawesome.com/66d41db8a2.js";
+</script>
+
+<main class="bg-black h-screen w-screen flex flex-row">
+    <nav class="flex flex-col">
+        <div class="flex flex-col flex-1 p-4 justify-center items-center gap-5">
+            <div class="aspect-square border-gray-700 border-[2px] bg-gray-900 p-4 text-2xl rounded-xl size-15 flex justify-center align-center">
+                <i class="fa-brands fa-github"></i>
+            </div>
+            <div class="aspect-square border-gray-700 border-[2px] bg-gray-900 p-4 text-2xl rounded-xl size-15 flex justify-center align-center">
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+            <div class="aspect-square border-gray-700 border-[2px] bg-gray-900 p-4 text-2xl rounded-xl size-15 flex justify-center align-center">
+                <i class="fa-solid fa-file-lines"></i>
+            </div>
+            <div class="aspect-square border-gray-700 border-[2px] bg-gray-900 p-4 text-2xl rounded-xl size-15 flex justify-center align-center">
+                <i class="fa-solid fa-envelope"></i>
+            </div>
+        </div>
+        <div class="h-px bg-gray-900 w-full"></div>
+        <div class="text-gray-500 text-xs p-4 text-center w-full">v0.1.0</div>
+    </nav>
+    <div class="w-px bg-gray-900 h-screen"></div>
+    <div class="flex justify-center items-center w-full h-full">
+        <div class="flex flex-col w-3xl border-gray-700 h-min border rounded-2xl">
+            <div class="flex items-center w-full px-5 py-2">
+                <div class="flex gap-3 w-full">
+                    <div class="size-[12px] bg-red-600 rounded-full"></div>
+                    <div class="size-[12px] bg-yellow-400 rounded-full"></div>
+                    <div class="size-[12px] bg-green-600 rounded-full"></div>
+                </div>
+                <div class="w-full bg-gray-900 rounded-sm border border-gray-700 text-xs text-gray-500 py-1 text-center tracking-wider">lucasjulien.com</div>
+                <div class="w-full"></div>
+            </div>
+            <div class="h-px bg-gray-700 w-full"></div>
+            <div class="flex flex-row size-full font-mono">
+                <div class="bg-gray-900 flex flex-col text-center gap-3 p-5 text-xs text-gray-400 rounded-bl-2xl">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                    <div>7</div>
+                    <div>8</div>
+                    <div>9</div>
+                    <div>10</div>
+                    <div>11</div>
+                    <div>12</div>
+                    <div>13</div>
+                    <div>14</div>
+                    <div>15</div>
+                </div>
+                <div class="bg-gray-950 p-5 text-xs flex flex-col gap-3 tracking-widest min-h-full w-full rounded-br-3xl text-white">
+                    <div>class <span class="text-green-400">Lucas</span></div>
+                    <div>{'{'}</div>
+                    <div class="ml-[25px]"><span class="text-blue-400">public static void</span> Main(<span class="text-blue-400">string</span>[] <span class="text-blue-400">args</span>)</div>
+                    <div class="ml-[25px]">{'{'}</div>
+                    <div class="ml-[50px]"><span class="text-blue-400">String</span>[] lucasLinks = <span class="text-blue-400">new string</span>[4];</div>
+                    <div class="ml-[50px]">lucasLinks[<span class="text-green-400">0</span>] = <span class="text-green-400">"My Blog posts"</span>;</div>
+                    <div class="ml-[50px]">lucasLinks[<span class="text-green-400">1</span>] = <span class="text-green-400">"My MacOS Configuration"</span>;</div>
+                    <div class="ml-[50px]">lucasLinks[<span class="text-green-400">2</span>] = <span class="text-green-400">"My definitive list of open source software"</span>;</div>
+                    <div class="ml-[50px]">lucasLinks[<span class="text-green-400">3</span>] = <span class="text-green-400">"How my brain works"</span>;</div>
+                    <div class="ml-[25px]">}</div>
+                    <div>}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
